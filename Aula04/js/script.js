42f7c045ac2fe02f309d4892926be85a245c9443
Felipepsii <felipe811801@gmail.com>
2022-04-06 12:25:56 -0300

    Atualização trabalho

---
+++ Aula04/js/script.js
@@ -0,0 +1,48 @@
+var num = 0;
+
+function umadez() {
+
+
+
+    let para = document.createElement('p');
+
+    para.textContent = num;
+
+    document.body.appendChild(para);
+
+    const buttons = document.querySelectorAll('button');
+
+
+
+    for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].addEventListener('click', createParagraph);
+
+    }
+
+    num++;
+
+    if (num >= 10) {
+        num = 10;
+    }
+
+
+}
+
+function debito() {
+    window.location.href = "debito.html";
+
+}
+
+function voltar() {
+    window.location.href = "index.html";
+}
+
+function jenkins() {
+    window.location.href = "https://www.gocache.com.br/dicas/o-que-e-jenkins-para-iniciantes/";
+}
+
+function sonar() {
+    window.location.href = "https://blog.myscrumhalf.com/sonar-apoiando-a-qualidade-do-desenvolvimento-de-software/"
+
+};
